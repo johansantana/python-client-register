05c453a9c007a569a93e00f1f382c3b2e11856db
Johan Santana <johancarlosst@gmail.com>
2023-11-20 22:20:46 -0400

    feat: added views for users

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-# Create your views here.
+from .forms import RegisterUserForm
+
+def register(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        # display blank registration form.
+        form = RegisterUserForm()
+    else:
+        # Process completed form.
+        form = RegisterUserForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in and then redirect to home page
+            login(request, new_user)
+            return redirect('clients:index')
+
+    # display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def logout(request):
+    return render(request, 'logged_out.html')
