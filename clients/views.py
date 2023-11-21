3b3cfdda0bed91cd500bb2d654abb9fb381038e3
Johan Santana <johancarlosst@gmail.com>
2023-11-20 21:18:05 -0400

    feat: created clientform

--- clients/views.py
+++ clients/views.py
@@ -11,4 +11,4 @@ def client(request, client_id):
     """Show a single client"""
     client = Client.objects.get(id=client_id)
     context = { 'client': client }
-    return render(request, 'client.html')
+    return render(request, 'client.html', context)
