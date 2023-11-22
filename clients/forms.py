24835c54aa6b916c681f7db53e3d10b53bbb4bd2
Johan Santana <johancarlosst@gmail.com>
2023-11-22 12:25:55 -0400

    fix: modified client form

--- clients/forms.py
+++ clients/forms.py
@@ -6,9 +6,10 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['first_name', 'last_name', 'username', 'email', 'phone_number']
-        labels = {
-            'first_name': '', 
-            'last_name': '', 
-            'username': '', 
-            'email': '',
-            'phone_number': '', }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Nombre', 'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Apellido', 'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Usuario', 'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Correo electrónico', 'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Numero telefónico', 'class': 'form-control'})
+        }
